447e073161e6fe4791bcb8c5547b19f016ac135e
Minh <minhau107@gmail.com>
2018-11-10 21:51:38 -0600

    Interpret flow output. Add serial commands to read values in test runs.

--- Flow_Sensor/Flow_Sensor.ino
+++ Flow_Sensor/Flow_Sensor.ino
@@ -8,18 +8,22 @@
 //SDA = A4; 
 const int address = 0x49; //default I2C address of flow sensor
 int flowValue;
-int fullScaleFlow;
+int flowOutput;
+int fullScaleFlow = 300; //takes the value of 10, 15, 20, 50, 100, 200, or 300. Must be specified to interpret the data
 int serialNo[4];
 int count = -1;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin(); //join I2C bus as master
+  Serial.begin(9600);
   delay(50); //allow flow sensor to perform warm up
   for (int i = 0; i<2; i++){
-    Wire.requestFrom(address, 2);
+    Wire.requestFrom(address, 2); //flow sensor responds with 2 bytes
+    Serial.print("Flow Sensor Serial Number:  ");
     while (Wire.available()){
-      serialNo[count++]= Wire.read();
+      serialNo[count++]= Wire.read(); //Read one byte a time
+      Serial.println(Wire.read()); //Read one byte a time
     } 
     delay(15); //command response time
   } //First 2 read sequences return serial number
@@ -27,8 +31,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Wire.requestFrom(address,2);
+  Wire.requestFrom(address,2); //flow sensor responds with 2 bytes
   if (bitRead(Wire.read(),15)==0 && bitRead(Wire.read(),16)==0){
-    flowValue = Wire.read();
-  } //The first ﻿two bits of each flow reading will be 00, bitRead() reads from rightmost bit
+    flowOutput = Wire.read();
+    Serial.print("Flow Ouput:  ");
+    Serial.println(flowOutput);
+    flowValue = fullScaleFlow*((flowOutput/16384)-0.1)/0.8; //equation given in the datasheet
+    Serial.print("Flow Applied:  ");
+    Serial.println(flowValue);
+  } //The first ﻿two bits of each valid flow reading will be 00, bitRead() reads from rightmost bit
+  else {
+    Serial.print("Error reading flow value!");
+  }
 }
