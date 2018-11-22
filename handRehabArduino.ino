82d6f2d4bdb47f56243a6272a46a58b83d36d7ae
Aaron Toderash (adt150) <aaron.t@usask.ca>
2018-11-21 18:31:59 -0600

    minor

--- handRehabArduino.ino
+++ handRehabArduino.ino
@@ -1,4 +1,4 @@
-//If not properly set up, go to Sketch > Include Library > Manage Library. Search for and install required libraries:
+// If not properly set up, go to Sketch > Include Library > Manage Library. Search for and install required libraries:
 #include <Wire.h> //library to control I2C bus from the flow sensor
 #include <Servo.h> //library to control the servo
 
