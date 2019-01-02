440406e2daaa896f27f8f4df66e517ee13ad83df
Minh <minhau107@gmail.com>
2019-01-01 21:45:26 -0600

    Update flow sensor files

--- Flow_Sensor/Flow_Sensor.ino
+++ Flow_Sensor/Flow_Sensor.ino
@@ -1,8 +1,6 @@
-#include <Wire.h>
-
 //Experimental file for flow sensor codes
-//If done and tested successfully, migrate to main files
-
+//Datasheet: https://www.mouser.ca/datasheet/2/187/honeywell-sensing-zephyr-digital-airflow-10-slpm-3-740283.pdf
+#include <Wire.h>
 //TWI pins location is predetermined by library
 //SCL = A5;
 //SDA = A4; 
@@ -10,7 +8,7 @@ const int address = 0x49; //default I2C address of flow sensor
 int flowValue;
 int flowOutput;
 int fullScaleFlow = 300; //takes the value of 10, 15, 20, 50, 100, 200, or 300. Must be specified to interpret the data
-int serialNo[4];
+char serialNo[4];
 int count = -1;
 
 void setup() {
@@ -22,8 +20,9 @@ void setup() {
     Wire.requestFrom(address, 2); //flow sensor responds with 2 bytes
     Serial.print("Flow Sensor Serial Number:  ");
     while (Wire.available()){
-      serialNo[count++]= Wire.read(); //Read one byte a time
-      Serial.println(Wire.read()); //Read one byte a time
+      char c = Wire.read(); //Read one byte a time
+      strcat(serialNo,c);
+      Serial.println(serialNo); //Read one byte a time
     } 
     delay(15); //command response time
   } //First 2 read sequences return serial number
@@ -32,15 +31,18 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Wire.requestFrom(address,2); //flow sensor responds with 2 bytes
-  if (bitRead(Wire.read(),15)==0 && bitRead(Wire.read(),16)==0){
-    flowOutput = Wire.read();
-    Serial.print("Flow Ouput:  ");
-    Serial.println(flowOutput);
-    flowValue = fullScaleFlow*((flowOutput/16384)-0.1)/0.8; //equation given in the datasheet
-    Serial.print("Flow Applied:  ");
-    Serial.println(flowValue);
-  } //The first ﻿two bits of each valid flow reading will be 00, bitRead() reads from rightmost bit
-  else {
-    Serial.print("Error reading flow value!");
+  while(Wire.available()){ 
+    char c = Wire.read();
+    strcat(flowOutput, c);
+    if (bitRead(flowOutput,15)==0 && bitRead(flowOutput,16)==0){
+      Serial.print("Flow Ouput:  ");
+      Serial.println(flowOutput);
+      flowValue = fullScaleFlow*((flowOutput/16384)-0.1)/0.8; //equation given in the datasheet
+      Serial.print("Flow Applied:  ");
+      Serial.println(flowValue);
+    } //The first ﻿two bits of each valid flow reading will be 00, bitRead() reads from rightmost bit
+    else {
+      Serial.print("Error reading flow value!");
+    }
   }
 }
