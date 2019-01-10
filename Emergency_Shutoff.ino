8a377782a91ea2c490fd332754117bd7c01b4b0b
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-01-09 20:53:58 -0600

    Emergency Shutoff now accepts a RESET signale

--- Emergency_Shutoff.ino
+++ Emergency_Shutoff.ino
@@ -3,10 +3,20 @@
  * The safest thing to do is shutoff the motor and pump, then open the valves.
  * We do this in a while loop forever to ensure the device is shutoff whenever
  * the button is pressed.
+ * 
+ * NOTE: RESETS and returns to 
  */
 void emergencyShutoff(){
   while(true){
-    Serial.println("Shuting off the motor");
+    if(Serial.available()){
+      char tempEmergencyInput = Serial.read();
+      if(tempEmergencyInput=='R'){
+        therapyMode = idleMode;
+        return;
+      }
+      // Else: Continue, since we only reset if we recieve an 'R'
+    }
+    Serial.print("Shuting off the motor");
     motorOff();
     delay(100);
     Serial.println("Opening finger and emergency valves.");
