390c5eafa701244c837e5fbc02dbf935b41c5325
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-02-20 17:03:15 -0600

    Bluetooth documentation in separate script

---
+++ BT.ino
@@ -0,0 +1,11 @@
+/**
+ * Bluetooth general notes:
+ * 
+ * Android app for development and sending Serial commands:
+ * Serial Bluetooth Terminal by Kai Morich
+ * App configuration:
+ *    TODO: Include an app configuration file in this repo.
+ *    
+ * 
+ * HC-05 password/pin: 1234
+ */
