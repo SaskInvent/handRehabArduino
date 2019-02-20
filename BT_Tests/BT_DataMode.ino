ee5af25a34bfd17dc4511938d1eb3bf8e7e94e58
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-02-20 17:31:15 -0600

    BT Docs etc.

---
+++ BT_Tests/BT_DataMode.ino
@@ -0,0 +1,47 @@
+// source: https://www.teachmemicro.com/arduino-bluetooth/#Control_a_LED_via_Bluetooth_and_Android
+# include <SoftwareSerial.h>
+
+/*
+ * Use this sketch to test a BT module after AT mode has been confirmed to work.
+ * pins:
+ *    - 12,11 see below
+ *    - Arduino 3.3V to HC-05 VCC
+ *    - Arduino GND to HC-05 GND
+ * 
+ * TODO: We are supposed to be able to upload sketchs to the Arduino using BT.  I was unable to get this working.
+ */
+
+// Uncomment the following example for a demonstration of how SoftwareSerial works.
+
+//
+//SoftwareSerial Bluetooth(12, 11); // RX, TX  (NOTE: RX is wired into HC-05 TXD,
+//                                  //                and TX is wired into HC-05 RXD)
+//                                  // RX = Recieve, TX = Transmission
+//int LED = 13; // The on-board LED
+//int Data; // Data recieved
+//
+//void setup(){
+//  Bluetooth.begin(9600);
+//  Serial.begin(9600);
+//  Serial.println("Waiting for command...");
+//  Bluetooth.println("Send 1 to turn on the LED.  Send 0 to turn OFF");
+//  pinMode(LED, OUTPUT);
+//}
+//
+//void loop(){
+//  if(Bluetooth.available()){ // Wait for data recieved
+//    Data=Bluetooth.read();
+//    if(Data=='1'){
+//      digitalWrite(LED,1);
+//      Serial.println("LED On!");
+//      Bluetooth.println("LED On!!");
+//    }
+//    else if(Data=='0'){
+//      digitalWrite(LED,0);
+//      Serial.println("LED Off!");
+//      Bluetooth.println("LED On D13 Off ! ");
+//    }
+//    else{;} // noop
+//  }
+//  delay(100);
+//}
