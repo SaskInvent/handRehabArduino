1c1746dce810d106012d5d9cbca8a6a2d649ce8c
Aaron Toderash (adt150) <aaron.t@usask.ca>
2019-02-20 17:55:13 -0600

    BT, Docs

--- BT.ino
+++ BT.ino
@@ -4,7 +4,13 @@
  * Android app for development and sending Serial commands:
  * Serial Bluetooth Terminal by Kai Morich
  * App configuration:
- *    TODO: Include an app configuration file in this repo.
+ *   1. See the 'BT_Tests/serial_bluetooth_terminal.cfg.txt' file.
+ *   2. Download the Serial Bluetooth Terminal app.
+ *   3. In the app:  corner three dots -> Configuration -> Import
+ *   4. Read the button descriptions.  You are ready to go!
+ * NOTE: This app is only for issueing commands to the Arduino.  
+ *       To view the Serial output for development connect the Arduino to a PC or RPi via
+ *       Serial port as usual.
  *    
  * 
  * HC-05 password/pin: 1234
